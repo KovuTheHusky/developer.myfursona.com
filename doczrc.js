8d74355d905edf4cb2ecf79250c84525bad6f4ef
KovuTheHusky <kovuthehusky@gmail.com>
2020-04-30 00:54:43 -0400

    Fix menu and ignore in configuration

--- doczrc.js
+++ doczrc.js
@@ -3,5 +3,10 @@ export default {
   themeConfig: {
     initialColorMode: 'dark',
     useColorSchemeMediaQuery: true
-  }
+  },
+  menu: [
+    'Home',
+    { name: 'Components', menu: ['Alert', 'Button'] }
+  ],
+  ignore: ['README.md', 'CHANGELOG.md', 'CODE_OF_CONDUCT.md', 'CONTRIBUTING.md', 'LICENSE.md']
 }
