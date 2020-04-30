93d6e7cfee177c606164ae507b00963dccb4a0ec
KovuTheHusky <kovuthehusky@gmail.com>
2020-04-30 00:58:07 -0400

    Fix page title in menu

--- doczrc.js
+++ doczrc.js
@@ -5,7 +5,7 @@ export default {
     useColorSchemeMediaQuery: true
   },
   menu: [
-    'Home',
+    'Getting Started',
     { name: 'Components', menu: ['Alert', 'Button'] }
   ],
   ignore: ['README.md', 'CHANGELOG.md', 'CODE_OF_CONDUCT.md', 'CONTRIBUTING.md', 'LICENSE.md']
